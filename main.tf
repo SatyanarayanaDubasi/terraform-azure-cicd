693c9c96d0dd3413b5bf4e5ea89fa550c6c6ce0b
Satyanarayana Dubasi <satya.dubasi91@gmail.com>
2026-05-12 15:39:17 +0530

    tags added for approval

--- main.tf
+++ main.tf
@@ -11,10 +11,13 @@ provider "azurerm" {
   features {}
   subscription_id = "2e22aec5-7840-45c5-a46b-2688971d3451"
 }
-
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location_name
+
+  tags = {
+    env = "prod"
+  }
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -22,7 +25,4 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location_name
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = var.vnet_address_space
-}
-tags = {
-  env = "prod"
 }
