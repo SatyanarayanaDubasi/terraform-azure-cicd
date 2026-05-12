4186485776a41a76613a459b8a07a9a46bac03c7
Satyanarayana Dubasi <satya.dubasi91@gmail.com>
2026-05-12 13:27:05 +0530

    Added default variable values

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "rg_name" {
-  type = string
+  type    = string
+  default = "cicd-rg"
 }
 
 variable "location_name" {
-  type = string
+  type    = string
+  default = "West Europe"
 }
 
 variable "vnet_name" {
-  type = string
+  type    = string
+  default = "cicd-vnet"
 }
 
 variable "vnet_address_space" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.0.0/16"]
 }
